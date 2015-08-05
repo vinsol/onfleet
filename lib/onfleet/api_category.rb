98c6529b88c5c940f90a4287324f9f921d27bd49
Isildy <isildy@mail.ru>
2015-08-05 17:00:19 +0300

    Update api_category.rb

--- lib/onfleet/api_category.rb
+++ lib/onfleet/api_category.rb
@@ -52,7 +52,7 @@ module Onfleet
     end
 
     def set_api_url
-      @api_url = URI.encode(@api_endpoint + @category_name)
+      @api_url = URI.encode(@api_endpoint + "/#{@category_name}")
     end
 
     def raise_error_with_message(parsed_response={})
